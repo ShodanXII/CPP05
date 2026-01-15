330a0ee02f714a27fc7c407882ad3ec51f886973
Abde Nnour Chat <achat@c1r6p6.1337.ma>
2026-01-15 16:37:38 +0100

    almost done ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,6 +1,39 @@
-#include <string>
+#include "Bureaucrat.hpp"
 
-int main()
+int	main(void)
 {
-    
+	try
+	{
+		Bureaucrat	b1(1, "Abdennour");
+		std::cout << b1 << std::endl;
+		b1.increment();
+		std::cout << b1 << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << "-----------------------------------" << std::endl;
+	try
+	{
+		Bureaucrat	b2(150 , "ooohhh ghi mandatory");
+		std::cout << b2 << std::endl;
+		b2.decremnt();
+		std::cout << b2 << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << "-----------------------------------" << std::endl;
+	try
+	{
+		Bureaucrat	b3(0 , "test");
+		std::cout << b3 << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	return (0);
 }
