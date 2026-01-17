75d15ba5f49d138261ca2dec34f7e834c560d549
unknown <abdourchatte99@gmail.com>
2026-01-17 02:58:44 +0100

    lol

--- ex02/PresidentialPardonForm.cpp
+++ ex02/PresidentialPardonForm.cpp
@@ -12,3 +12,33 @@ PresidentialPardonForm::PresidentialPardonForm( const std::string& _target) : AF
     std::cout << AForm::getName() << " constractor been called from PresidentialPardonForm" << std::endl;
 }
 
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& copy)
+: AForm(copy), target(copy.target)
+{
+    std::cout << AForm::getName() << " copy constructor been called from PresidentialPardonForm" << std::endl;
+}
+
+PresidentialPardonForm& PresidentialPardonForm::operator=(const PresidentialPardonForm& copy)
+{
+    if (this != &copy)
+    {
+        AForm::operator=(copy);
+        target = copy.target;
+    }
+    std::cout << AForm::getName() << " copy assignment operator been called from PresidentialPardonForm" << std::endl;
+    return (*this);
+}
+
+PresidentialPardonForm::~PresidentialPardonForm()
+{
+    std::cout << AForm::getName() << " destructor been called from PresidentialPardonForm" << std::endl;
+}
+
+void PresidentialPardonForm::execute(Bureaucrat const & executor) const
+{
+    if (!getIsSigned())
+        throw std::runtime_error("PresidentialPardonForm not signed");
+    if (executor.getGrade() > getExecGrade())
+        throw AForm::GradeTooLowException();
+    std::cout << target << " has been pardoned by Zaphod Beeblebrox." << std::endl;
+}
